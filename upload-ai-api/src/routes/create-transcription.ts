9545eb1d507d7367d882bb9a5ed9fbf8b86d42d5
metzevandro <metzxevandro@gmail.com>
2023-09-17 13:59:22 -0300

    Aula 02 - Avançando o back-end da aplicação

--- upload-ai-api/src/routes/create-transcription.ts
+++ upload-ai-api/src/routes/create-transcription.ts
@@ -47,6 +47,8 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
       }
     })
 
-    return response
+    return {
+      transcription,
+    }
   })
 }
--- upload-ai-api/src/routes/upload-video.ts
+++ upload-ai-api/src/routes/upload-video.ts
@@ -1,16 +1,15 @@
 import { FastifyInstance } from "fastify";
 import { fastifyMultipart } from "@fastify/multipart";
-import path from "path";
-import { randomUUID } from "crypto";
-import fs from "fs";
-import { pipeline } from "stream";
-import { promisify } from "util";
+import path from "node:path";
+import { randomUUID } from "node:crypto";
+import fs from "node:fs";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
 import { prisma } from "../lib/prisma";
 
-const pump = promisify(pipeline);
+const pump = promisify(pipeline)
 
 export async function uploadVideoRoute(app: FastifyInstance) {
-  
   app.register(fastifyMultipart, {
     limits: {
       fileSize: 1_048_576 * 25 // 25mb
@@ -39,12 +38,12 @@ export async function uploadVideoRoute(app: FastifyInstance) {
     const video = await prisma.video.create({
       data: {
         name: data.filename,
-        path: uploadDestination,
+        path: uploadDestination
       }
     })
 
     return {
-      video,
+      video
     }
   })
 }
